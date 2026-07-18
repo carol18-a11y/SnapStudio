86821edb84d6bf4af6cb3af361d61a8021020974
carol18-a11y <carolnoronha618@gmail.com>
2026-07-18 15:51:09 +0530

    built core photobooth camera experience

--- static/script.js
+++ static/script.js
@@ -11,6 +11,8 @@ const restartButton = document.getElementById("restart");
 const canvas = document.getElementById("photo");
 const timer = document.getElementById("timer");
 const progress = document.getElementById("progress");
+const flash = document.getElementById("flash");
+const shutterSound = new Audio("/static/sounds/freesound_community-camera-shutter-6305.mp3")
 
 // Camera Setup
 
@@ -39,15 +41,15 @@ button.addEventListener("click", () => {
 
         count--;
         timer.innerHTML = count;
+        timer.style.animation = "none";
+        timer.offsetHeight;
+        timer.style.animation = "pop 0.5s";
 
         if (count === 0) {
 
             clearInterval(countdown);
 
             takePhoto();
-
-            timer.innerHTML = "Photo Taken!";
-
         }
 
     }, 1000);
@@ -60,11 +62,11 @@ restartButton.addEventListener("click",() => {
 
 
 
-
-
 // Capture Photo Function
-
 function takePhoto() {
+    flashScreen();
+    shutterSound.currentTime=0;
+    shutterSound.play();
 
     if (photoCount >= maxPhotos) {
         return;
@@ -75,6 +77,9 @@ function takePhoto() {
 
     const context = canvas.getContext("2d");
 
+    context.setTransform(1,0,0,1,0,0);
+    context.translate(canvas.width, 0);
+    context.scale(-1,1);
     context.drawImage(
         video,
         0,
@@ -115,11 +120,20 @@ function takePhoto() {
 
 }
 
+function flashScreen(){
+    console.log("Flash!");
+    flash.style.opacity = "0.8";
+    setTimeout(() => {
+        flash.style.opacity = "0";
+    },1000);
+}
+
 function restartSession(){
     photoCount =0;
     gallery.innerHTML ="";
     progress.innerHTML=`Photo 1 of ${maxPhotos}`;
-    timer.innnerHTML = "";
+    timer.innerHTML = "";
     button.disabled=false;
     button.innerHTML="Take Photo";
 }
+
